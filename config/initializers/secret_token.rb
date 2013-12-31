505dabf1bf5f757da4eacdf4ffb7f26d1d7d4970
U-GRIFFWin7x64PC\Griff <Griff@GRIFFWin7x64PC.(none)>
2013-12-31 00:20:46 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '168fa0a424ba21a6b95d032c3273589a816174bfc279c4a5f6fdc41c369a71e85938e72914e8acfeaeae2cee9808abe1623f08e7ef6861f38380271260c7c80d'
